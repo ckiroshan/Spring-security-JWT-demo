fddbbc85b4a1ab74efbd7244032e06930775e364
ckiroshan <ckiroshan@gmail.com>
2024-03-21 21:59:30 +0530

    Initial commit

---
+++ src/test/java/com/kiroshan/jwt/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiroshan.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
